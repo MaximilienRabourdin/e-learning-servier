9f75e6ae0f7648340e0f17c66b0795aa5d8ab4a9
Maximilien Rabourdin <maximilien.rabourdin@gmail.com>
2022-10-20 16:36:15 +0200

    sidebar integration + title WIP

--- e-learning/src/components/CentralBox/CentralBoxStyled.js
+++ e-learning/src/components/CentralBox/CentralBoxStyled.js
@@ -1,17 +1,73 @@
 import styled from "styled-components";
 
 const CentralBoxStyled = styled.div `
-background: blue;
-width: 80vw;
-height: 60vh
-display:flex;
-align-items:center;
-align-content:center;
+width: 100vw;
+height: 100vh;
+position:fixed;
+
 
 .all-box{
+    background: white;
+    margin: 4rem 8rem;
+    height: 84vh;
+    width: 80vw;
+    display:flex;
+    filter: drop-shadow(0px 7px 36px rgba(0, 89, 148, 0.24));
+    border-radius: 10px;
+};
+
+// Navbar start
+
+.navbar {
+    width: 6rem;
+    height:100%;
+    background-color:#0089E3;
+    border-radius: 10px 0px 0px 10px;
+};
+
+.all_button {
+    margin:0;
+    padding:0;
     width:100%;
-    height:100%
+    list-style:none;
+    display:flex;
+    align-items:center;
+    flex-direction:column;
+    align-content:center;
 }
+
+.button{
+    display:flex;
+    align-items:center;
+    flex-direction:center;
+    align-content:center;
+}
+
+a {
+    width:100%;
+    height: 100%;
+}
+
+.right_section {
+    width: 100%
+    background-color:red;
+}
+
+.title_page{
+    background-image:url("../../../public/background_color_title.svg");
+    color: #005994;
+    padding: 2rem; 
+    margin: 0rem;
+    font-family: 'Abel', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 40px;
+    z-index: 9999;
+}
+
+
+// Navbar End
+
 `
 
 export default CentralBoxStyled;
--- e-learning/src/components/CentralBox/index.js
+++ e-learning/src/components/CentralBox/index.js
@@ -4,7 +4,34 @@ import CentralBoxStyled from "./CentralBoxStyled";
 function CentralBox() {
   return (
     <CentralBoxStyled>
-      <div className="all-box">CentralBox</div>
+      <div className="all-box">
+        <nav className="navbar">
+        <ul className="all_button">
+          <li className="button">
+            <a href='../../public/menu_icon.svg'></a>
+            <p>Menu</p>
+          </li>
+          <li className="button">
+            <a href='../../menu_icon.svg'></a>
+            <p>Menu</p>
+          </li>
+          <li className="button">
+            <a href='../../public/menu_icon.svg'></a>
+            <p>Menu</p>
+          </li>
+          <li className="button">
+            <a href='../../../public/menu_icon.svg'></a>
+            <p>Menu</p>
+          </li>
+        </ul>
+
+        </nav>
+        <div className="right_section"> 
+        <div className="title_section">
+        <p className="title_page"> Présentation du cas clinique interactif </p>
+        </div>
+        </div>
+      </div> 
     </CentralBoxStyled>
   );
 }
